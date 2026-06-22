833c01604e377346e404035887631b052f29d8fc
Arti <arti.matrix12@gmail.com>
2026-06-22 21:03:28 +0530

    Add boolean logic examples in day_2_boolens.py

---
+++ day_2_boolens.py
@@ -0,0 +1,21 @@
+# the Doorman
+print("lets get started")
+age=12
+is_allowed= age >= 18
+print(is_allowed)
+
+#Stock Checker
+items_in_stock=0
+is_out_of_stock= items_in_stock ==0
+print(is_out_of_stock)
+
+#toggle switch
+light_switch=True
+light_switch=not light_switch
+print(light_switch)
+
+#All together
+rider_height=135
+has_ticket=True
+can_ride= (rider_height >=120) and (has_ticket== True)
+print(can_ride)

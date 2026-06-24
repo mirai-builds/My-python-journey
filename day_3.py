a6ffce29a4cd1359e8bd802bf59f7d75bc037d4c
Arti <arti.matrix12@gmail.com>
2026-06-24 20:38:22 +0530

    Create day_3.py

---
+++ day_3.py
@@ -0,0 +1,16 @@
+print("lets go for day 3")
+customer_name='aari'
+total_boxes=0
+total_boxes=0
+cart_total+=45
+cart_total+=25
+total_boxes+=3
+"""floor division (//) to get only full trucks cutting of any decimals
+e.g=43//20 the ans wil be 2
+"""
+trucks_needed=total_boxes//2
+leftover_boxes=total_boxes%2  #modulo operator shows whats left behind
+print("Customer Name:", customer_name)
+print("Final Cart Total: $", cart_total)
+print("Full Trucks Dispatched:", trucks_needed)
+print("Boxes Left Behind:", leftover_boxes)

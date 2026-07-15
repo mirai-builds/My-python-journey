8ee8acdf6f5524155b98f4feccde947605e09d37
Arti <arti.matrix12@gmail.com>
2026-07-15 13:08:53 +0530

    Add bill split calculator to day_5.py

--- day_5.py
+++ day_5.py
@@ -1,1 +1,7 @@
 print("DAY 5") #lets dive into if-else logic
+print("Bill Split Calculator")
+Bill_amount = float(input())
+Tip_percentage = float(input())
+Tip_amount=(Tip_percentage / 100) * Bill_amount
+total__amount= Bill_amount + Tip_amount
+print(total__amount)

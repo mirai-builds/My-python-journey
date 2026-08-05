c4fec825fa4304638527c5df82dcd4f5e41d0840
Arti <arti.matrix12@gmail.com>
2026-08-05 17:30:02 +0530

    Correct loop syntax and logic in day7.py
    Fix syntax errors in while, for, and continue loops.

---
+++ day7.py
@@ -0,0 +1,17 @@
+# while loop 
+number = float (input())   
+while numbers/=2
+number/=2
+print (numbers)
+
+#Break 
+for i range(1,11)   #stpos the further execution immeditaly
+if i==6 :
+  Break
+  print(i)
+
+#continue
+from i in range (1,9)    # it stops current interaction and countinous to next intergration
+if i==4
+continue 
+print (i)
